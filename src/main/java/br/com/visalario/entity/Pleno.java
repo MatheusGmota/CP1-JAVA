1cd088d3c834ad5d4f113111f70d30bf9e924ad6
felipeSeiki <felipe.hashiguti@gmail.com>
2025-03-19 18:35:42 -0300

    Criando a subclasse Pleno

---
+++ src/main/java/br/com/visalario/entity/Pleno.java
@@ -0,0 +1,9 @@
+package br.com.visalario.entity;
+
+public class Pleno extends Funcionario{
+
+    public Pleno(Integer id, String nome, double valorHora, double horasTrab, double salario, String cargo) {
+        super(id, nome, valorHora, horasTrab, salario, cargo);
+    }
+
+}
