b8c6da952c3c26169b8f93646606c2e64dccbf5b
felipeSeiki <felipe.hashiguti@gmail.com>
2025-03-19 18:35:28 -0300

    Criando a subclasse Estagiario

---
+++ src/main/java/br/com/visalario/entity/Estagiario.java
@@ -0,0 +1,9 @@
+package br.com.visalario.entity;
+
+public class Estagiario extends Funcionario{
+
+    public Estagiario(Integer id, String nome, double valorHora, double horasTrab, double salario, String cargo) {
+        super(id, nome, valorHora, horasTrab, salario, cargo);
+    }
+
+}
