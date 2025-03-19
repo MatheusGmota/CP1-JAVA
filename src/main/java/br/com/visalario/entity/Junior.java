57dfe852008032c05b86b6c764d3e50b714492ec
felipeSeiki <felipe.hashiguti@gmail.com>
2025-03-19 18:35:37 -0300

    Criando a subclasse Junior

---
+++ src/main/java/br/com/visalario/entity/Junior.java
@@ -0,0 +1,9 @@
+package br.com.visalario.entity;
+
+public class Junior extends  Funcionario{
+
+    public Junior(Integer id, String nome, double valorHora, double horasTrab, double salario, String cargo) {
+        super(id, nome, valorHora, horasTrab, salario, cargo);
+    }
+
+}
