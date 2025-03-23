21c6a7c6a428700c37c1c1384382feb7336abb3a
MatheusGmota <livematgreg@gmail.com>
2025-03-23 18:37:52 -0300

    Teste unitario para classe GeradorSql

---
+++ src/main/java/br/com/visalario/view/TesteGeradorSql.java
@@ -0,0 +1,13 @@
+package br.com.visalario.view;
+
+import br.com.visalario.entity.GeradorSql;
+import br.com.visalario.entity.Senior;
+
+public class TesteGeradorSql {
+    public static void main(String[] args) {
+        Senior jose = new Senior(1, "José", 20, 160, "Senior" );
+        jose.calcularSalario();
+
+        new GeradorSql().gerarSqlInsert(jose);
+    }
+}
